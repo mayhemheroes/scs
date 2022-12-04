29e81d7a14990ce9bb5cb4b7f87d22cd8fcea484
Raj Shah <rnshah9@asu.edu>
2022-12-04 22:55:16 +0000

    Add harness

---
+++ mayhem/fuzz_scs_norm_sq.cpp
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <climits>
+
+#include "FuzzedDataProvider.h"
+#include "linalg.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    FuzzedDataProvider provider(data, size);
+    scs_float f = provider.ConsumeFloatingPoint<scs_float>();
+    _scs_norm_sq(&f, 1);
+
+    return 0;
+}
